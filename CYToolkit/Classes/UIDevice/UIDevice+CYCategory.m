aac4ed55ae63acd04183f208151a2cb65682fbf7
Chris <340019109@qq.com>
2020-04-02 09:12:48 +0800

    【功能新增】设备对应关系更新

--- CYToolkit/Classes/UIDevice/UIDevice+CYCategory.m
+++ CYToolkit/Classes/UIDevice/UIDevice+CYCategory.m
@@ -155,6 +155,9 @@ static NSString *cyLogPhoneTokenKeychainKey = @"cyLogPhoneTokenKeychainKey";
         || [preciseType isEqualToString:@"iPhone11,4"]
         || [preciseType isEqualToString:@"iPhone11,6"]
         || [preciseType isEqualToString:@"iPhone11,8"]
+        || [preciseType isEqualToString:@"iPhone12,1"]
+        || [preciseType isEqualToString:@"iPhone12,3"]
+        || [preciseType isEqualToString:@"iPhone12,5"]
         ) {
         return YES;
     }
@@ -168,7 +171,52 @@ static NSString *cyLogPhoneTokenKeychainKey = @"cyLogPhoneTokenKeychainKey";
 if ([preciseType isEqualToString:appleStr]) { return publicStr; }
 
 + (NSString *)iphoneAnnounceTypeWithPreciseType:(NSString *)preciseType {
+    
+    NSString *transType = nil;
+    
+    transType = [self __iphoneAnnounceTypeWithPreciseType:preciseType];
+    if (nil != transType) {
+        return transType;
+    }
+    
+    transType = [self __ipodAnnounceTypeWithPreciseType:preciseType];
+    if (nil != transType) {
+        return transType;
+    }
+    
+    transType = [self __ipadAnnounceTypeWithPreciseType:preciseType];
+    if (nil != transType) {
+        return transType;
+    }
+    
+    transType = [self __ipadAirAnnounceTypeWithPreciseType:preciseType];
+    if (nil != transType) {
+        return transType;
+    }
 
+    transType = [self __ipadProAnnounceTypeWithPreciseType:preciseType];
+    if (nil != transType) {
+        return transType;
+    }
+
+    transType = [self __ipadMiniAnnounceTypeWithPreciseType:preciseType];
+    if (nil != transType) {
+        return transType;
+    }
+
+    transType = [self __simulatorAnnounceTypeWithPreciseType:preciseType];
+    if (nil != transType) {
+        return transType;
+    }
+    
+    return preciseType;
+}
+
+#define __typePair(appleStr, publicStr) \
+if ([preciseType isEqualToString:appleStr]) { return publicStr; }
+
++ (NSString *)__iphoneAnnounceTypeWithPreciseType:(NSString *)preciseType {
+    
     __typePair(@"iPhone1,1", @"iPhone 2G")
     __typePair(@"iPhone1,2", @"iPhone 3G")
     __typePair(@"iPhone2,1", @"iPhone 3GS")
@@ -183,13 +231,15 @@ if ([preciseType isEqualToString:appleStr]) { return publicStr; }
     __typePair(@"iPhone6,1", @"iPhone 5s")
     __typePair(@"iPhone6,2", @"iPhone 5s")
     
-    __typePair(@"iPhone7,1", @"iPhone 6 Plus")
     __typePair(@"iPhone7,2", @"iPhone 6")
+    __typePair(@"iPhone7,1", @"iPhone 6 Plus")
     __typePair(@"iPhone8,1", @"iPhone 6s")
     __typePair(@"iPhone8,2", @"iPhone 6s Plus")
     __typePair(@"iPhone8,4", @"iPhone SE")
     __typePair(@"iPhone9,1", @"iPhone 7")
+    __typePair(@"iPhone9,3", @"iPhone 7")
     __typePair(@"iPhone9,2", @"iPhone 7 Plus")
+    __typePair(@"iPhone9,4", @"iPhone 7 Plus")
     
     __typePair(@"iPhone10,1", @"iPhone 8")
     __typePair(@"iPhone10,4", @"iPhone 8")
@@ -197,63 +247,122 @@ if ([preciseType isEqualToString:appleStr]) { return publicStr; }
     __typePair(@"iPhone10,5", @"iPhone 8 Plus")
     __typePair(@"iPhone10,3", @"iPhone X")
     __typePair(@"iPhone10,6", @"iPhone X")
+    __typePair(@"iPhone11,8", @"iPhone XR")
     __typePair(@"iPhone11,2", @"iPhone XS")
     __typePair(@"iPhone11,4", @"iPhone XS Max")
     __typePair(@"iPhone11,6", @"iPhone XS Max")
-    __typePair(@"iPhone11,8", @"iPhone XR")
     
-    __typePair(@"iPod1,1", @"iPod Touch 1G")
+    __typePair(@"iPhone12,1", @"iPhone 11")
+    __typePair(@"iPhone12,3", @"iPhone 11 Pro")
+    __typePair(@"iPhone12,5", @"iPhone 11 Pro Max")
+        
+    return nil;
+}
+
++ (NSString *)__ipodAnnounceTypeWithPreciseType:(NSString *)preciseType {
+    
+    __typePair(@"iPod1,1", @"iPod Touch")
     __typePair(@"iPod2,1", @"iPod Touch 2G")
     __typePair(@"iPod3,1", @"iPod Touch 3G")
     __typePair(@"iPod4,1", @"iPod Touch 4G")
     __typePair(@"iPod5,1", @"iPod Touch 5G")
+    __typePair(@"iPod7,1", @"iPod Touch 6G")
+    __typePair(@"iPod9,1", @"iPod Touch 7G")
+
+    return nil;
+}
+
++ (NSString *)__ipadAnnounceTypeWithPreciseType:(NSString *)preciseType {
     
     __typePair(@"iPad1,1", @"iPad 1G")
-    __typePair(@"iPad2,1", @"iPad 2")
-    __typePair(@"iPad2,2", @"iPad 2")
-    __typePair(@"iPad2,3", @"iPad 2")
-    __typePair(@"iPad2,4", @"iPad 2")
-    __typePair(@"iPad2,5", @"iPad Mini 1G")
-    __typePair(@"iPad2,6", @"iPad Mini 1G")
-    __typePair(@"iPad2,7", @"iPad Mini 1G")
-    __typePair(@"iPad3,1", @"iPad 3")
-    __typePair(@"iPad3,2", @"iPad 3")
-    __typePair(@"iPad3,3", @"iPad 3")
-    __typePair(@"iPad3,4", @"iPad 4")
-    __typePair(@"iPad3,5", @"iPad 4")
-    __typePair(@"iPad3,6", @"iPad 4")
+    __typePair(@"iPad2,1", @"iPad 2G")
+    __typePair(@"iPad2,2", @"iPad 2G")
+    __typePair(@"iPad2,3", @"iPad 2G")
+    __typePair(@"iPad2,4", @"iPad 2G")
+    __typePair(@"iPad3,1", @"iPad 3G")
+    __typePair(@"iPad3,2", @"iPad 3G")
+    __typePair(@"iPad3,3", @"iPad 3G")
+    __typePair(@"iPad3,4", @"iPad 4G")
+    __typePair(@"iPad3,5", @"iPad 4G")
+    __typePair(@"iPad3,6", @"iPad 4G")
+
+    __typePair(@"iPad6,11", @"iPad 5G")
+    __typePair(@"iPad6,12", @"iPad 5G")
+    __typePair(@"iPad7,5", @"iPad 6G")
+    __typePair(@"iPad7,6", @"iPad 6G")
+    __typePair(@"iPad7,11", @"iPad 7G")
+    __typePair(@"iPad7,12", @"iPad 7G")
+    
+    return nil;
+}
+
++ (NSString *)__ipadAirAnnounceTypeWithPreciseType:(NSString *)preciseType {
     
     __typePair(@"iPad4,1", @"iPad Air")
     __typePair(@"iPad4,2", @"iPad Air")
     __typePair(@"iPad4,3", @"iPad Air")
+    __typePair(@"iPad5,3", @"iPad Air 2G")
+    __typePair(@"iPad5,4", @"iPad Air 2G")
+    __typePair(@"iPad11,3", @"iPad Air 3G")
+    __typePair(@"iPad11,4", @"iPad Air 3G")
+    
+    return nil;
+}
+
++ (NSString *)__ipadProAnnounceTypeWithPreciseType:(NSString *)preciseType {
+    
+    __typePair(@"iPad6,3", @"iPad Pro 9.7-inch")
+    __typePair(@"iPad6,4", @"iPad Pro 9.7-inch")
+    
+    __typePair(@"iPad7,3", @"iPad Pro 10.5-inch")
+    __typePair(@"iPad7,4", @"iPad Pro 10.5-inch")
+    
+    __typePair(@"iPad8,1", @"iPad Pro 11.0-inch")
+    __typePair(@"iPad8,2", @"iPad Pro 11.0-inch")
+    __typePair(@"iPad8,3", @"iPad Pro 11.0-inch")
+    __typePair(@"iPad8,4", @"iPad Pro 11.0-inch")
+    
+    __typePair(@"iPad6,7", @"iPad Pro 12.9-inch")
+    __typePair(@"iPad6,8", @"iPad Pro 12.9-inch")
+    __typePair(@"iPad7,1", @"iPad Pro 12.9-inch 2G")
+    __typePair(@"iPad7,2", @"iPad Pro 12.9-inch 2G")
+    __typePair(@"iPad8,5", @"iPad Pro 12.9-inch 3G")
+    __typePair(@"iPad8,6", @"iPad Pro 12.9-inch 3G")
+    __typePair(@"iPad8,7", @"iPad Pro 12.9-inch 3G")
+    __typePair(@"iPad8,8", @"iPad Pro 12.9-inch 3G")
+            
+    return nil;
+}
+
++ (NSString *)__ipadMiniAnnounceTypeWithPreciseType:(NSString *)preciseType {
+    
+    __typePair(@"iPad2,5", @"iPad Mini")
+    __typePair(@"iPad2,6", @"iPad Mini")
+    __typePair(@"iPad2,7", @"iPad Mini")
     __typePair(@"iPad4,4", @"iPad Mini 2G")
     __typePair(@"iPad4,5", @"iPad Mini 2G")
     __typePair(@"iPad4,6", @"iPad Mini 2G")
-    __typePair(@"iPad4,7", @"iPad mini 3")
-    __typePair(@"iPad4,8", @"iPad mini 3")
-    __typePair(@"iPad4,9", @"iPad mini 3")
     
-    __typePair(@"iPad5,1", @"iPad mini 4")
-    __typePair(@"iPad5,2", @"iPad mini 4")
-    __typePair(@"iPad5,3", @"iPad Air 2")
-    __typePair(@"iPad5,4", @"iPad Air 2")
-    __typePair(@"iPad6,7", @"iPad Pro 12.9")
-    __typePair(@"iPad6,8", @"iPad Pro 12.9")
-    __typePair(@"iPad6,3", @"iPad Pro 9.7")
-    __typePair(@"iPad6,4", @"iPad Pro 9.7")
-    __typePair(@"iPad6,11", @"iPad 5")
-    __typePair(@"iPad6,12", @"iPad 5")
-    __typePair(@"iPad7,1", @"iPad Pro 12.9 inch 2nd gen")
-    __typePair(@"iPad7,2", @"iPad Pro 12.9 inch 2nd gen")
-    __typePair(@"iPad7,3", @"iPad Pro 10.5")
-    __typePair(@"iPad7,4", @"iPad Pro 10.5")
-    __typePair(@"iPad7,5", @"iPad 6")
-    __typePair(@"iPad7,6", @"iPad 6")
+    __typePair(@"iPad4,7", @"iPad mini 3G")
+    __typePair(@"iPad4,8", @"iPad mini 3G")
+    __typePair(@"iPad4,9", @"iPad mini 3G")
+    __typePair(@"iPad5,1", @"iPad mini 4G")
+    __typePair(@"iPad5,2", @"iPad mini 4G")
+    __typePair(@"iPad11,1", @"iPad mini 5G")
+    __typePair(@"iPad11,2", @"iPad mini 5G")
     
     __typePair(@"i386", @"iPhone Simulator")
     __typePair(@"x86_64", @"iPhone Simulator")
     
-    return preciseType;
+    return nil;
+}
+
++ (NSString *)__simulatorAnnounceTypeWithPreciseType:(NSString *)preciseType {
+    
+    __typePair(@"i386", @"iPhone Simulator")
+    __typePair(@"x86_64", @"iPhone Simulator")
+
+    return nil;
 }
 
 + (NSString *)networkStrFromSystemNetworkType:(NSInteger)systemNetType {
